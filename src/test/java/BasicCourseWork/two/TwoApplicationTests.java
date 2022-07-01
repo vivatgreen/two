95975aa2ffea318c470034992002418a037dad9c
vivatgreen <grindsasha@bk.ru>
2022-07-01 22:33:56 +0300

    Course_work

---
+++ src/test/java/BasicCourseWork/two/TwoApplicationTests.java
@@ -0,0 +1,13 @@
+package BasicCourseWork.two;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
